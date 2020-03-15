1642d33d00c4083d3b1d132e68de4d12f4c32f1c
Marcos Huck <marcos.h3012@gmail.com>
2020-03-15 10:43:34 -0300

    Add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User {
+  public uuid: string;
+  public firstName: string;
+  public middleName: string;
+  public lastName: string;
+  public email: string;
+  public birthday: Date;
+  public role: number;
+}
